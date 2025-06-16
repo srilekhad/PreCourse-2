53b0f7c3c9db140269f7157b3f943f4c5d4c1ca5
srilekhad <slkdavuluri@gmail.com>
2025-06-16 16:48:12 -0500

    Done precourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,8 +3,27 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
-    } 
+        while(l<=r)
+        {
+            int mid = (l+r)/2;
+            if(arr[mid] == x)
+            {
+                return mid;
+            }
+            if(arr[mid]>x)
+            {
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        } 
+        return -1;
   
+
+        }
+        
     // Driver method to test above 
     public static void main(String args[]) 
     { 
